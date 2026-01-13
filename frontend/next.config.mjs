4e82bdf30de087e9c7d03f77383db62b3d24523d
KwonYongBin <dydqls6789@naver.com>
2026-01-13 18:08:04 +0900

    11

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -8,11 +8,11 @@ const nextConfig = {
           source: '/api/:path*',
           destination: 'http://teamproject-bicycleapp.duckdns.org/api/:path*',
         },
-        // CSRF 관련 주소도 rewrite에 추가하세요
-        {
-          source: '/csrf/:path*',
-          destination: 'http://teamproject-bicycleapp.duckdns.org/csrf/:path*',
-        },
+//         // CSRF 관련 주소도 rewrite에 추가하세요
+//         {
+//           source: '/csrf/:path*',
+//           destination: 'http://teamproject-bicycleapp.duckdns.org/csrf/:path*',
+//         },
   ];
     },
 };
